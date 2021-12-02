aa8a9a86a4f6560971a08ec6cdfacb4b3b09b595
Aldy Victor Rianes <aldyvictor1046@gmail.com>
2021-12-02 10:40:10 +0700

    Try Redis

--- app/Http/Controllers/LatihanRedisController.php
+++ app/Http/Controllers/LatihanRedisController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
 
 class LatihanRedisController extends Controller
 {
-    //
+    public function testredis()
+    {
+        Redis::set('nama', 'RedisTest');
+         return Redis::get('nama');
+    }
+
+    public function cache()
+    {
+        $data = array(
+            'blog' => 'RedisTest',
+            'author' => 'Aldy Victor Rianes'
+        );
+
+        // if (Cache::has('nama')) {
+        //     Cache::add('nama', json_encode($data));
+        // }
+
+        return Cache::get($data);
+    }
 }
